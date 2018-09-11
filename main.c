93762df9d0ad7636b5a709436a30ba412a0ca929
Tristan Henning <tristan@customcrypto.com>
2018-09-11 15:30:30 -0700

    Added infinite user loop

--- main.c
+++ main.c
@@ -12,6 +12,33 @@
 **     save results to CSV file
 */
 
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
 typedef struct Race{
     //User Inputs
     float start_lap;
@@ -95,7 +122,7 @@ int bestResult(float *results, int length_of_array, int initial_interval, int en
     return best_time_pos;
 }
 
-int main()
+void runProgram()
 {
     Race *race = malloc(sizeof(Race));
 
@@ -137,9 +164,13 @@ int main()
     //printRace(race);
 
     free(race);
+}
 
-    //WINDOWS SPECIFIC
-    system("PAUSE");
+int main()
+{
+    while(1){
+        runProgram();
+    }
 
     return 0;
 }
