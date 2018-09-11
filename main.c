6244ae3298c77cd2b84ccc83bfd55ff0cb5cd9cb
Tristan Henning <tristan@customcrypto.com>
2018-09-11 00:07:47 -0700

    Edited TODO

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 **     [DONE]implement runSim() into main()
 **     [DONE]output best time
 **     [DONE]debug main(). There is an issue :(
-**     add logic to cancel pit stop if within 5 laps
+**     [DONE]add logic to cancel pit stop if within 5 laps
 **     save results to CSV file
 */
 
